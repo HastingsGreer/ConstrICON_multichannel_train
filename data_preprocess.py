f0b27df9afb7676010c6e7069ec0394b4dc53b74
Thomas Greer <tgreer@biag-gpu0.cs.unc.edu>
2024-03-22 11:44:37 -0400

    initial commit

---
+++ data_preprocess.py
@@ -0,0 +1,23 @@
+import itk
+import numpy as np
+import glob
+import torch
+import tqdm
+
+output = []
+
+for pth in tqdm.tqdm(glob.glob("data/degree_powers_normalized_dipy/degree_power_images/*")):
+    print(pth)
+    img = itk.imread(pth)
+    img = np.array(img)
+    print(img.dtype)
+    img = torch.tensor(img)[None]
+    output.append(img)
+
+output = torch.cat(output)
+
+print(output.shape)
+
+import footsteps
+
+torch.save(output, footsteps.output_dir + "train_tensor.trch")
