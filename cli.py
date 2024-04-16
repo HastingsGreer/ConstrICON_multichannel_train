76aacee0858ae3721840334763022a2eb71d516c
hastings <hastings.greer@gmail.com>
2024-04-16 13:17:10 -0400

    multichannel_image_details

--- cli.py
+++ cli.py
@@ -37,11 +37,25 @@ def get_model():
     return net
 
 def preprocess(image):
-    image = itk.CastImageFilter[type(image), itk.Image[itk.F, 3]].New()(image)
     max_ = np.max(np.array(image))
-    image = itk.shift_scale_image_filter(image, shift=0., scale = 1 / max_)
-    
+
+    arv = itk.GetArrayViewFromImage(image)
+    arv /= max_
+
     return image
+
+def to_image_list(image):
+    arr = itk.array_from_image(image)
+
+    ret = []
+
+    for i in range(4):
+        im = itk.image_from_array(arr[i])
+        im.SetOrigin(np.array(image.GetOrigin())[:3])
+        im.SetSpacing(np.array(image.GetSpacing())[:3])
+        im.SetDirection(np.array(image.GetDirection())[:3, :3])
+        ret.append(im)
+    return ret
 
 if __name__ == "__main__":
     import itk
@@ -51,28 +65,58 @@ if __name__ == "__main__":
     parser.add_argument("--moving")
     parser.add_argument("--transform_out")
     parser.add_argument("--warped_moving_out", default=None)
+    parser.add_argument("--spacing_debug", action='store_true')
+    parser.add_argument("--io_iterations", required=False,
+                         default="None", help="The number of IO iterations. Default is 50. Set to 'None' to disable IO.")
 
     args = parser.parse_args()
+
+    if args.io_iterations == "None":
+        io_iterations = None
+    else:
+        io_iterations = int(args.io_iterations)
 
     net = get_model()
 
     fixed = itk.imread(args.fixed)
     moving = itk.imread(args.moving)
 
-    phi_AB, phi_BA = icon_registration.itk_wrapper.register_pair(net,preprocess(moving), preprocess(fixed), finetune_steps=50)
+    fixed = preprocess(fixed)
+    moving = preprocess(moving)
+
+    if args.spacing_debug:
+        print(fixed.GetSpacing())
+        print(fixed.GetDirection())
+        print(fixed.GetOrigin())
+        print(fixed.GetLargestPossibleRegion().GetSize())
+
+    fixed = to_image_list(fixed)
+    moving = to_image_list(moving)
+
+    if args.spacing_debug:
+        print(fixed[0].GetSpacing())
+        print(fixed[0].GetDirection())
+        print(fixed[0].GetOrigin())
+        print(fixed[0].GetLargestPossibleRegion().GetSize())
+
+
+    phi_AB, phi_BA = icon_registration.itk_wrapper.register_pair_with_multimodalities(net,moving, fixed, finetune_steps=io_iterations)
 
     itk.transformwrite([phi_AB], args.transform_out)
 
-    if args.warped_moving_out:
-        interpolator = itk.LinearInterpolateImageFunction.New(moving)
+    if args.warped_moving_out and args.spacing_debug:
+        interpolator = itk.LinearInterpolateImageFunction.New(moving[0])
         warped_image_A = itk.resample_image_filter(
-                moving,
+                moving[0],
                 transform=phi_AB,
                 interpolator=interpolator,
                 use_reference_image=True,
-                reference_image=fixed
+                reference_image=fixed[0]
                 )
         itk.imwrite(warped_image_A, args.warped_moving_out)
+    if args.spacing_debug:
+        itk.imwrite(fixed[0], "fixed_processed.mha")
+        itk.imwrite(moving[0], "moving_processed.mha")
 
 
 
